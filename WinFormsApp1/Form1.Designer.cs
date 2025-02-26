29b9a4e09da018bcbef0718d1b7eca43d80fcfd8
Tran Nguyen Xuan Khanh <trannguyenxuankhanh15@gmail.com>
2025-02-26 17:08:01 +0700

    2

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tableLayoutPanel2 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            linkLabel2 = new LinkLabel();
+            linkLabel1 = new LinkLabel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBoxPasswd = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            textBoxEmail = new TextBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 145F));
+            tableLayoutPanel2.Controls.Add(button1, 0, 0);
+            tableLayoutPanel2.Controls.Add(button2, 1, 0);
+            tableLayoutPanel2.Location = new Point(492, 289);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(288, 37);
+            tableLayoutPanel2.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(146, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(536, 259);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(148, 24);
+            checkBox1.TabIndex = 15;
+            checkBox1.Text = "Hiển thị mật khẩu";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(558, 335);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(109, 20);
+            linkLabel2.TabIndex = 14;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Quên mật khẩu";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(567, 315);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(0, 20);
+            linkLabel1.TabIndex = 13;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22.6470585F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 77.35294F));
+            tableLayoutPanel1.Controls.Add(textBoxPasswd, 1, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
+            tableLayoutPanel1.Controls.Add(textBoxEmail, 1, 0);
+            tableLayoutPanel1.Location = new Point(404, 152);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(379, 101);
+            tableLayoutPanel1.TabIndex = 12;
+            // 
+            // textBoxPasswd
+            // 
+            textBoxPasswd.Location = new Point(88, 53);
+            textBoxPasswd.Name = "textBoxPasswd";
+            textBoxPasswd.Size = new Size(288, 27);
+            textBoxPasswd.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Email";
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.Location = new Point(88, 3);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.Size = new Size(288, 27);
+            textBoxEmail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(558, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 46);
+            label1.TabIndex = 11;
+            label1.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(66, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(257, 394);
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(checkBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "LOGIN";
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxEmail;
+        private TextBox textBoxPasswd;
+        private TableLayoutPanel tableLayoutPanel1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private CheckBox checkBox1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void textBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPasswd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
