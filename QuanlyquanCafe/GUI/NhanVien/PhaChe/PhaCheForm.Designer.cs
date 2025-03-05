380319e52a025bf222ff7b47715996b9a6eb0319
Diem Phung <phungb2112000@student.ctu.edu.vn>
2025-03-05 13:38:05 +0700

    Pha che form

---
+++ QuanlyquanCafe/GUI/NhanVien/PhaChe/PhaCheForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace QuanlyquanCafe.GUI.NhanVien.PhaChe
+{
+    partial class PhaCheForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OrderID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TableNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IsPrepared = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isNotified = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IsPaid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pha Chế";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh sách đơn hàng";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OrderID,
+            this.TableNumber,
+            this.IsPrepared,
+            this.isNotified,
+            this.IsPaid});
+            this.dataGridView1.Location = new System.Drawing.Point(41, 126);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 227);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // OrderID
+            // 
+            this.OrderID.HeaderText = "Order ID";
+            this.OrderID.MinimumWidth = 6;
+            this.OrderID.Name = "OrderID";
+            this.OrderID.Width = 125;
+            // 
+            // TableNumber
+            // 
+            this.TableNumber.HeaderText = "Số bàn";
+            this.TableNumber.MinimumWidth = 6;
+            this.TableNumber.Name = "TableNumber";
+            this.TableNumber.Width = 125;
+            // 
+            // IsPrepared
+            // 
+            this.IsPrepared.HeaderText = "Đã pha chế";
+            this.IsPrepared.MinimumWidth = 6;
+            this.IsPrepared.Name = "IsPrepared";
+            this.IsPrepared.Width = 125;
+            // 
+            // isNotified
+            // 
+            this.isNotified.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.isNotified.HeaderText = "Đã thông báo";
+            this.isNotified.MinimumWidth = 6;
+            this.isNotified.Name = "isNotified";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Đã pha chế";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(387, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Đã thanh toán";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // IsPaid
+            // 
+            this.IsPaid.HeaderText = "Đã thanh toán";
+            this.IsPaid.MinimumWidth = 6;
+            this.IsPaid.Name = "IsPaid";
+            this.IsPaid.Width = 125;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(230, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Đã thông báo";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(558, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 49);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Làm mới";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // PhaCheForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PhaCheForm";
+            this.Text = "PhaCheForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TableNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IsPrepared;
+        private System.Windows.Forms.DataGridViewTextBoxColumn isNotified;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IsPaid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
