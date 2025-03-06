95f0ab34b2e5a986a52c8df616af31a0a2c7c08d
Nguyễn Tấn Lộc <locb2111807@student.ctu.edu.vn>
2025-03-06 12:37:29 +0700

    Modify Admin Interface and Add Data Provider SingleTon

---
+++ QuanlyquanCafe/Admin/DAO/DataProvider.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanlyquanCafe.Admin.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider singletonInstance;
+
+
+
+        private string connStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=RestaurantManagement;Integrated Security=True;TrustServerCertificate=True";
+
+        public static DataProvider SingletonInstance { 
+            get { if (singletonInstance == null) singletonInstance = new DataProvider(); return DataProvider.singletonInstance; } 
+            private set => DataProvider.singletonInstance = value; 
+        }
+
+        private DataProvider() {}
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(data);
+                conn.Close();
+            }
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = cmd.ExecuteScalar();
+                conn.Close();
+            }
+            return data;
+        }
+
+
+    }
+}
--- QuanlyquanCafe/Program.cs
+++ QuanlyquanCafe/Program.cs
@@ -17,7 +17,7 @@ namespace QuanlyquanCafe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fAdmin());
         }
     }
 }
