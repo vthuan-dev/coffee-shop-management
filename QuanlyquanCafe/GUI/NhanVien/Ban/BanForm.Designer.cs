bc89b7c8bbc8f4c5e171a2a14010a2399b290dbd
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2025-03-22 16:26:17 +0700

    adding sth'

--- QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.Designer.cs
+++ QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.Designer.cs
@@ -29,12 +29,23 @@
         private void InitializeComponent()
         {
             this.panelButtons = new System.Windows.Forms.Panel();
-            this.datbanBtn = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.datbanBtn = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
             this.panelButtons.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -53,25 +64,15 @@
             this.panelButtons.TabIndex = 0;
             this.panelButtons.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
             // 
-            // datbanBtn
+            // button8
             // 
-            this.datbanBtn.Location = new System.Drawing.Point(15, 26);
-            this.datbanBtn.Name = "datbanBtn";
-            this.datbanBtn.Size = new System.Drawing.Size(100, 72);
-            this.datbanBtn.TabIndex = 0;
-            this.datbanBtn.Text = "Đặt bàn";
-            this.datbanBtn.UseVisualStyleBackColor = true;
-            this.datbanBtn.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(15, 130);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 72);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Gợp bàn";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button8.Location = new System.Drawing.Point(15, 328);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 72);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Thanh toán";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button4_Click);
             // 
             // button7
             // 
@@ -83,19 +84,40 @@
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button8
+            // button4
             // 
-            this.button8.Location = new System.Drawing.Point(15, 328);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(100, 72);
-            this.button8.TabIndex = 0;
-            this.button8.Text = "Thanh toán";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Location = new System.Drawing.Point(15, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 72);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Gợp bàn";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // datbanBtn
+            // 
+            this.datbanBtn.Location = new System.Drawing.Point(15, 26);
+            this.datbanBtn.Name = "datbanBtn";
+            this.datbanBtn.Size = new System.Drawing.Size(100, 72);
+            this.datbanBtn.TabIndex = 0;
+            this.datbanBtn.Text = "Đặt bàn";
+            this.datbanBtn.UseVisualStyleBackColor = true;
+            this.datbanBtn.Click += new System.EventHandler(this.button4_Click);
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.button9);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Controls.Add(this.button10);
+            this.flowLayoutPanel1.Controls.Add(this.button11);
+            this.flowLayoutPanel1.Controls.Add(this.button12);
+            this.flowLayoutPanel1.Controls.Add(this.button13);
+            this.flowLayoutPanel1.Controls.Add(this.button14);
+            this.flowLayoutPanel1.Controls.Add(this.button15);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(136, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -111,9 +133,141 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(179, 101);
             this.button9.TabIndex = 0;
-            this.button9.Text = "button9";
+            this.button9.Text = "Bàn 1";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button1.Location = new System.Drawing.Point(198, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bàn 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button2.Location = new System.Drawing.Point(383, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 101);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Bàn 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button3.Location = new System.Drawing.Point(13, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 101);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Bàn 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button5.Location = new System.Drawing.Point(198, 120);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 101);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Bàn 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button6.Location = new System.Drawing.Point(383, 120);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(179, 101);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Bàn 1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button10.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button10.Location = new System.Drawing.Point(13, 227);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(179, 101);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "Bàn 1";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button11.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button11.Location = new System.Drawing.Point(198, 227);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(179, 101);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "Bàn 1";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button12.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button12.Location = new System.Drawing.Point(383, 227);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(179, 101);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "Bàn 1";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button13.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button13.Location = new System.Drawing.Point(13, 334);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(179, 101);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "Bàn 1";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button14.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button14.Location = new System.Drawing.Point(198, 334);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(179, 101);
+            this.button14.TabIndex = 0;
+            this.button14.Text = "Bàn 1";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button15.Image = global::QuanlyquanCafe.Properties.Resources._7113274;
+            this.button15.Location = new System.Drawing.Point(383, 334);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(179, 101);
+            this.button15.TabIndex = 0;
+            this.button15.Text = "Bàn 1";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button9_Click);
             // 
             // BanForm
             // 
@@ -138,5 +292,16 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
     }
 }
--- QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.cs
+++ QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.cs
@@ -13,35 +13,68 @@ namespace QuanlyquanCafe.GUI.NhanVien.Ban
 {
     public partial class BanForm : Form
     {
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button5;
-        private Button button6;
-        private Label label1;
-        private Panel panelTables;
-
         public BanForm()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             this.Dock = DockStyle.Fill;
+            
+            // Đăng ký sự kiện resize
+            button9.Resize += new EventHandler(Button9_Resize);
+            
+            // Tạo các button bàn
+            CreateTableButtons();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ResizeButtonImage();
+            CreateTableButtons();
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
-
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+        }
+        
+        // Thêm phương thức xử lý sự kiện panel1_Paint_1
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            // Xử lý vẽ panel nếu cần
+        }
 
+        // Thêm phương thức xử lý sự kiện button4_Click
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Xác định button nào đã được nhấp
+            Button clickedButton = sender as Button;
+            
+            if (clickedButton != null)
+            {
+                // Kiểm tra text của button để xác định hành động
+                switch (clickedButton.Text)
+                {
+                    case "Đặt bàn":
+                        new BookTableForm().ShowDialog();
+                        break;
+                    case "Gợp bàn":
+                        new CombineTableForm().ShowDialog();
+                        break;
+                    case "Thanh toán":
+                        new PaymentForm().ShowDialog();
+                        break;
+                }
+            }
+        }
+
+        // Thêm phương thức xử lý sự kiện button9_Click
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn đã chọn " + ((Button)sender).Text);
+            // Hiển thị thông tin bàn hoặc menu khi click vào bàn
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -51,7 +84,6 @@ namespace QuanlyquanCafe.GUI.NhanVien.Ban
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -79,17 +111,20 @@ namespace QuanlyquanCafe.GUI.NhanVien.Ban
             try
             {
                 // Lấy hình ảnh từ Resources với tên đúng
-                Image originalImage = Properties.Resources._7113274;
+                Image originalImage = Properties.Resources.ResourceManager.GetObject("7113274") as Image;
                 
-                // Tạo một bản thu nhỏ theo kích thước button
-                Image resizedImage = new Bitmap(originalImage, button9.Width - 10, button9.Height - 10);
-                
-                // Gán cho button
-                button9.Image = resizedImage;
-                
-                // Đảm bảo vị trí hình ảnh đúng
-                button9.ImageAlign = ContentAlignment.MiddleCenter;
-                button9.TextImageRelation = TextImageRelation.ImageAboveText;
+                if (originalImage != null && button9 != null)
+                {
+                    // Tạo một bản thu nhỏ theo kích thước button
+                    Image resizedImage = new Bitmap(originalImage, button9.Width - 10, button9.Height - 10);
+                    
+                    // Gán cho button
+                    button9.Image = resizedImage;
+                    
+                    // Đảm bảo vị trí hình ảnh đúng
+                    button9.ImageAlign = ContentAlignment.MiddleCenter;
+                    button9.TextImageRelation = TextImageRelation.ImageAboveText;
+                }
             }
             catch (Exception ex)
             {
@@ -100,6 +135,72 @@ namespace QuanlyquanCafe.GUI.NhanVien.Ban
         private void Button9_Resize(object sender, EventArgs e)
         {
             ResizeButtonImage();
+        }
+
+        private void CreateTableButtons()
+        {
+            // Xóa các button hiện có trong flowLayoutPanel1 (nếu cần)
+            flowLayoutPanel1.Controls.Clear();
+            
+            // Lấy hình ảnh từ Resources
+            Image tableImage = Properties.Resources.ResourceManager.GetObject("7113274") as Image;
+            
+            // Điều chỉnh kích thước nhỏ hơn của button bàn
+            int buttonWidth = 140;  // Giảm từ 179
+            int buttonHeight = 85;  // Giảm từ 101
+            
+            // Tăng số lượng cột trong FlowLayoutPanel
+            flowLayoutPanel1.Padding = new Padding(5);
+            
+            // Tạo 12 bàn (có thể điều chỉnh số lượng)
+            for (int i = 1; i <= 12; i++)
+            {
+                Button btnTable = new Button();
+                btnTable.Name = "btnTable" + i;
+                btnTable.Text = "Bàn " + i;
+                btnTable.Size = new Size(buttonWidth, buttonHeight);
+                btnTable.Font = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                btnTable.UseVisualStyleBackColor = true;
+                btnTable.Margin = new Padding(8); // Thêm khoảng cách giữa các button
+                
+                // Thiết lập hình ảnh
+                if (tableImage != null)
+                {
+                    // Tạo bản thu nhỏ của hình ảnh
+                    Image resizedImage = new Bitmap(tableImage, buttonWidth - 30, buttonHeight - 25);
+                    btnTable.Image = resizedImage;
+                    
+                    // Định dạng hiển thị hình ảnh
+                    btnTable.ImageAlign = ContentAlignment.TopCenter;
+                    btnTable.TextAlign = ContentAlignment.BottomCenter;
+                    btnTable.TextImageRelation = TextImageRelation.ImageAboveText;
+                }
+                
+                // Tùy chỉnh giao diện để làm đẹp hơn
+                btnTable.FlatStyle = FlatStyle.Flat;
+                btnTable.FlatAppearance.BorderSize = 1;
+                btnTable.FlatAppearance.BorderColor = Color.LightGray;
+                btnTable.BackColor = Color.White;
+                
+                // Đăng ký sự kiện Click
+                btnTable.Click += new EventHandler(table_Click);
+                
+                // Thêm vào flowLayoutPanel
+                flowLayoutPanel1.Controls.Add(btnTable);
+            }
+            
+            // Đảm bảo flowLayoutPanel có đủ không gian hiển thị tất cả button
+            flowLayoutPanel1.AutoScroll = true;
+        }
+
+        private void table_Click(object sender, EventArgs e)
+        {
+            Button clickedTable = sender as Button;
+            if (clickedTable != null)
+            {
+                MessageBox.Show("Bạn đã chọn " + clickedTable.Text);
+                // Thêm code xử lý khi chọn bàn
+            }
         }
     }
 }
