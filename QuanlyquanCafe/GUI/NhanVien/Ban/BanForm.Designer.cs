726f9f13de299666ce97f1596b414d13f90710bd
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2025-03-22 14:39:20 +0700

    temp task:

--- QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.Designer.cs
+++ QuanlyquanCafe/GUI/NhanVien/Ban/BanForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // BanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Quản lý bàn";
+            this.Name = "BanForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
